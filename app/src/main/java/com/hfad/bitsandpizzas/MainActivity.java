afabd649550971587fb54742b39151868419ffa8
SuperBigBang <superbigbang@yandex.ru>
2018-09-14 23:27:16 +0900

    14.09.2018 23:27

--- app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
@@ -1,11 +1,17 @@
 package com.hfad.bitsandpizzas;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
+
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ShareActionProvider;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private ShareActionProvider shareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,17 +22,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main,menu); //Заполнение меню. Если они есть, то добавляются на панель действий
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        shareActionProvider= (ShareActionProvider) menuItem.getActionProvider();
+        setIntent("This is example text");
         return super.onCreateOptionsMenu(menu);
+    }
+
+    private void setIntent(String text){
+        Intent intent= new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT,text);
+        shareActionProvider.setShareIntent(intent);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_create_order:
-                System.out.println("Нажата кнопка действия"); break;
+          //      System.out.println("Нажата кнопка действия"); //break не нужны т.к. нужно вернуть булево значение
+                Intent intent = new Intent(this,OrderActivity.class);
+                startActivity(intent);
+                return true;
             case R.id.action_settings:
-                System.out.println("Нажаты настройки"); break;
+           //     System.out.println("Нажаты настройки");
+                return true;
+                default: return super.onOptionsItemSelected(item); //если ни один не подходит то выполняется дефолт.
         }
-        return super.onOptionsItemSelected(item);
     }
 }
