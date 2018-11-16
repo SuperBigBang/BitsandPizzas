a2653aeab868f0c8194724e8d7d04efb1c59ffe2
SuperBigBang <superbigbang@yandex.ru>
2018-11-17 00:16:57 +0900

    feat: PizzaMaterialFragment onClick listener realize (on RecyclerView)

--- app/src/main/java/com/hfad/bitsandpizzas/CaptionedImagesAdapter.java
+++ app/src/main/java/com/hfad/bitsandpizzas/CaptionedImagesAdapter.java
@@ -4,6 +4,7 @@ import android.graphics.drawable.Drawable;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -11,7 +12,32 @@ import android.widget.TextView;
 class CaptionedImagesAdapter extends RecyclerView.Adapter<CaptionedImagesAdapter.ViewHolder> {
     private String[] captions;
     private int[] imageIds;
+    private Listener listener;
 
+    public void setListener(Listener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        CardView cardView = holder.cardView;
+        ImageView imageView = cardView.findViewById(R.id.info_image);
+        Drawable drawable = cardView.getResources().getDrawable(imageIds[position]);
+        imageView.setImageDrawable(drawable);
+        imageView.setContentDescription(captions[position]);
+        TextView textView = cardView.findViewById(R.id.info_text);
+        textView.setText(captions[position]);
+        //Заполнение заданного представления данными
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) {
+                    listener.onClick(position);
+                }
+            }
+        });
+
+    }
     public CaptionedImagesAdapter(String[] captions, int[] imageIds) {
         this.captions = captions;
         this.imageIds = imageIds;
@@ -25,16 +51,8 @@ class CaptionedImagesAdapter extends RecyclerView.Adapter<CaptionedImagesAdapter
         return new ViewHolder(cv);
     }   //Создание нового представления
 
-    @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        CardView cardView = holder.cardView;
-        ImageView imageView = cardView.findViewById(R.id.info_image);
-        Drawable drawable = cardView.getResources().getDrawable(imageIds[position]);
-        imageView.setImageDrawable(drawable);
-        imageView.setContentDescription(captions[position]);
-        TextView textView = cardView.findViewById(R.id.info_text);
-        textView.setText(captions[position]);
-        //Заполнение заданного представления данными
+    public interface Listener {
+        void onClick(int position);
     }
 
     @Override
